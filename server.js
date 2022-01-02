f970897bda032b58c22a2dd09c67f2ab787eb44c
mrWD <lvigtor@gmail.com>
2022-01-02 16:02:39 +0600

    Add server.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+
+  next();
+});
+
+app.use(express.static('./dist/daily-timer/'));
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/daily-timer/' });
+});
+
+app.listen(process.env.PORT || 8080);
